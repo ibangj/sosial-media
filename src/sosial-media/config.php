5312c3adfd90a90b60ca014d9e9a303484d0e32d
ibangj <getarjati96@gmail.com>
2021-11-09 16:44:58 +0700

    docker image ready

--- src/sosial-media/config.php
+++ src/sosial-media/config.php
@@ -1,7 +1,7 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "devopscilsy";
+$db_host = "db-sosialmedia.cgeajbxy0cgb.ap-southeast-1.rds.amazonaws.com";
+$db_user = "admin";
 $db_pass = "1234567890";
 $db_name = "dbsosmed";
 
